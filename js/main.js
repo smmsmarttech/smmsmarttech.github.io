3ef1379e0f093fbe4d27ed78b6906998e76d37e9
smmsmarttech <109649859+smmsmarttech@users.noreply.github.com>
2022-07-21 14:41:05 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function aler(){
 
 
      if(value == 1){
-          document.getElementById("paynow").href = "https://rzp.io/l/T41fDod9K6";
+          document.getElementById("paynow").href = "https://rzp.io/l/a8tj6aZ5";////https://rzp.io/l/T41fDod9K6
      }else if(value == 2){
           document.getElementById("paynow").href = "https://rzp.io/l/K1XZe4R";
      }
@@ -18,8 +18,40 @@ function aler(){
           document.getElementById("paynow").href = "https://rzp.io/l/1z56Ba1";
      }
      else if(value == 4){
-          document.getElementById("paynow").href = "https://rzp.io/l/JlVcHq9";
+          document.getElementById("paynow").href = "https://rzp.io/l/T41fDod9K6";
      }
+     else if(value == 5){
+          document.getElementById("paynow").href = "ttps://rzp.io/l/OhV5tn4B";
+     }
+
+     else if(value == 6){
+          document.getElementById("paynow").href = "https://rzp.io/l/4kGxqssoH";
+     }
+     else if(value == 7){
+          document.getElementById("paynow").href = "https://rzp.io/l/5X4fYRI6F";
+     }
+     else if(value == 8){
+          document.getElementById("paynow").href = "https://rzp.io/l/vr4MDfOyO";
+     }
+     else if(value == 9){
+          document.getElementById("paynow").href = "https://rzp.io/l/tDdc9jL4X";
+     }
+     
+
+
+     else if(value == 10){
+          document.getElementById("paynow").href = "https://rzp.io/l/qbar37Qa";
+     }
+     else if(value == 11){
+          document.getElementById("paynow").href = "https://rzp.io/l/sQEbuJ0wp";
+     }
+     else if(value == 12){
+          document.getElementById("paynow").href = "https://rzp.io/l/0coIrDyz";
+     }
+     else if(value == 13){
+          document.getElementById("paynow").href = "https://rzp.io/l/d7n8lDYB";
+     }
+     
      
 }
 
@@ -34,5 +66,43 @@ function money(){
     }else if(value == 4){
         return 90;
    }
+   else if(value == 5){
+     return 1000;
+
+   }
+   else if(value == 6){
+     return 1800;
+
+   }
+   else if(value == 7){
+     return 2300;
+
+   }
+   else if(value == 8){
+     return 120;
+
+   }
+   else if(value == 9){
+     return 190;
+
+   }
+   else if(value == 10){
+     return 250;
+
+   }
+   else if(value == 11){
+     return 220;
+
+   }
+   else if(value == 12){
+     return 300;
+
+   }
+   else if(value == 12){
+     return 160;
+
+   }
+   
+   
 }
 //https://api.whatsapp.com/send?phone=9390558027&text=Hi
